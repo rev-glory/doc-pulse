368f4e0a578db03c89368d668ac3567c8885a4ab
Sasta-Sharabi <153608735+Sasta-Sharabi@users.noreply.github.com>
2026-06-22 13:47:11 +0530

    scaffold NestJS application

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,1 +1,13 @@
-export {};
+import 'reflect-metadata';
+
+import { NestFactory } from '@nestjs/core';
+
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const port = process.env.PORT ?? 3000;
+  await app.listen(port);
+}
+
+void bootstrap();
